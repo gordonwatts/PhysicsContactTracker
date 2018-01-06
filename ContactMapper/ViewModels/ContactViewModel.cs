4a3c1cf1c9665a6cff19af30db8d49f97f655179
Gordon Watts <gordon@gordonandpaula.net>
2018-01-06 01:26:21 -0800

    Can display a contact name in a list

---
+++ ContactMapper/ViewModels/ContactViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Caliburn.Micro;
+using ContractTrackerInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactMapper.ViewModels
+{
+    /// <summary>
+    /// All information for a contact
+    /// </summary>
+    public class ContactViewModel : Screen
+    {
+        /// <summary>
+        /// Contact we will be showing.
+        /// </summary>
+        private readonly IContact _contact;
+
+        public ContactViewModel(IContact contact)
+        {
+            _contact = contact;
+        }
+
+        /// <summary>
+        /// Return the name for easy display in a view.
+        /// </summary>
+        public string Name => $"{_contact.LastName}, {_contact.FirstName}";
+    }
+}
--- ContactMapper/ViewModels/MainViewModel.cs
+++ ContactMapper/ViewModels/MainViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Linq;
 
 using Caliburn.Micro;
 
 using ContactMapper.Helpers;
+using InSpireHEPAccess;
 
 namespace ContactMapper.ViewModels
 {
@@ -10,6 +12,21 @@ namespace ContactMapper.ViewModels
     {
         public MainViewModel()
         {
+            People = new BindableCollection<ContactViewModel>();
+            LoadDummyData();
         }
+
+        private async void LoadDummyData()
+        {
+            // Add a simple contact (this will be "improved" when we have somethign real).
+            var finder = new InSpireContactFinder();
+            var me = await finder.FindContactAsync(new Uri("http://inspirehep.net/record/983968?ln=en"));
+            People.Add(new ContactViewModel(me.First()));
+        }
+
+        /// <summary>
+        /// List of all contacts we are tracking.
+        /// </summary>
+        public BindableCollection<ContactViewModel> People { get; private set; }
     }
 }
